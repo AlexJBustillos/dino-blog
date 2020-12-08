666e081ce0a26e2e04f65cf32d9aadaade3c59fa
alexjbustillos <alexjbustillos@gmail.com>
2020-12-08 12:24:41 -0500

    dinos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-
+import Dino from './Dino'
 import './App.css';
 
 function App(props) {
-  console.log(props);
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>{props.post.title}</h1>
-        <p>by {props.post.author}</p>
-        <p>by {props.post.body}</p>
-        <h2>Comment</h2>
-        <p>by {props.post.comments[0]}</p>
-      </header>
+      <Dino 
+        title={props.post.title}
+        author={props.post.author}
+        body={props.post.body}
+        comments={props.post.comments}
+      />
+      
     </div>
   );
 }
